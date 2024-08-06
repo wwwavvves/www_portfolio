f438400f7b5ae5a41846dde8d0235546a93c2f7f
Diana <wwwavvves@gmail.com>
2024-08-06 20:32:51 +0100

    Added education and workshops sections

--- hamb-menu.js
+++ hamb-menu.js
@@ -3,13 +3,11 @@ const btnToClose = document.querySelector("#btn-close");
 const navMenu = document.querySelector(".nav-menu");
 
 function openMenu() {
-  btnToOpen.setAttribute("aria-expanded", "true");
   navMenu.style.translate = 0;
 }
 
 function closeMenu() {
-    btnToClose.setAttribute("aria-expanded", "false");
-    navMenu.style.translate = "100vw 0";
+  navMenu.style.translate = "100vw 0";
 }
 
 btnToOpen.addEventListener("click", openMenu);
