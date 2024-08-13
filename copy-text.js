161e32fc8fbdfc8bef067f28a864d9488a478ad1
Diana <wwwavvves@gmail.com>
2024-08-13 20:02:07 +0100

    Added images to work section

--- copy-text.js
+++ copy-text.js
@@ -1,15 +1,8 @@
-function copyText() {
-  let text = document.getElementById("email");
-  let btn = document.getElementById("copy-btn");
+function copyEmail() {
+  let copySvg = document.getElementById("copy-btn");
 
   // Copy the text inside the text field
   navigator.clipboard.writeText("wwwavvves@gmail.com");
 
   // Change button text
-  if (btn.innerText === "Copy") {
-    btn.innerText = "Copied";
-  } else {
-    btn.innerText = "Copy";
-    navigator.clipboard.writeText("");
-  }
 }
